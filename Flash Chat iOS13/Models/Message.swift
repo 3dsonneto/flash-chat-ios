2acb1927467451a7782f7c4c760129592e8455a3
Edson <edson_neto1996@hotmail.com>
2021-03-15 15:15:12 -0300

    Adicionando TableView e inserindo dados na cell

---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Edson Neto on 15/03/21.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String //email
+    let body: String
+}
